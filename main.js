559ecce2bae968fa719454fda4e877886d237224
Asti <astistateofmind@gmail.com>
2023-01-30 11:01:05 -0800

    adds even or odd function

--- main.js
+++ main.js
@@ -17,3 +17,11 @@ function solution(number) {
     }
     return result
 }
+
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function evenOrOdd(number) {
+    if (number % 2 === 0) {
+        return 'Even'
+    } else return 'Odd'
+}
