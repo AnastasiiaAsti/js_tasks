b6f1c40cc8a36e1fed86ec4ffe4a9a6fdb58e05c
Asti <astistateofmind@gmail.com>
2023-01-30 15:28:32 -0800

    adds 2 options fro simple multiply func

--- main.js
+++ main.js
@@ -121,3 +121,17 @@ function otherAngle(a, b) {
 }
 
 const otherAngle = (a, b) => 180 - a - b
+
+// multiplying a given number by eight if it is an even number and by nine otherwise.
+
+function simpleMultiplication(number) {
+    if (number % 2 === 0) {
+        return number * 8
+    } else {
+        return number * 9
+    }
+}
+
+function simpleMultiplication2(n){
+  return n % 2 == 0 ? n * 8 : n * 9
+}
