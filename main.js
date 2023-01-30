9a86b662e5054454366da0718c96ac0145822d03
Asti <astistateofmind@gmail.com>
2023-01-30 15:05:52 -0800

    adds comment to one liner

--- main.js
+++ main.js
@@ -93,11 +93,18 @@ const quarterOf = (month) => {
         return 1
     } else if (month >= 4 && month <= 6) {
         return 2
-    } else if (month>=7 && month<=9){
+    } else if (month >=7 && month<=9){
         return 3
     } else {
         return 4
     }
 }
 
-const quarterOf2 = m => Math.ceil(m/3);
+
+// So that means, for months 1 - 3, dividing by 3 will give a value that will “have a ceiling” of 1
+// (1 / 3 = 0.3333, 2 / 3 = 0.666, 3 / 3 = 1).Once you hit 4 is where you get “exactly what you want” 
+// which is that 4 - 6 will have a “ceiling” of 2 for “the second quarter”
+// (4 / 3 = 1.333, 5 / 3 = 1.6666, 6 / 3 = 2).
+const quarterOf2 = m => Math.ceil(m / 3);
+
+
