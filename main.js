ca8291c4c4dbc9766ce7c9fe6bcfe46eb47015c9
Asti <astistateofmind@gmail.com>
2023-01-30 16:02:29 -0800

    adds reverse str func

--- main.js
+++ main.js
@@ -135,3 +135,15 @@ function simpleMultiplication(number) {
 function simpleMultiplication2(n){
   return n % 2 == 0 ? n * 8 : n * 9
 }
+
+// Complete the solution so that it reverses all of the words within the string passed in.
+
+// Words are separated by exactly one space and there are no leading or trailing spaces.
+
+// Example(Input --> Output):
+// "The greatest victory is that which requires no battle" -- >
+//     "battle no requires which that is victory greatest The"
+
+function reverseWords(str) {
+    return str.split(' ').reverse().join(' ')
+}
