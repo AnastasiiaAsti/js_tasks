fbd58652fb15dab554c408253e40e4938c9c0e68
Asti <astistateofmind@gmail.com>
2023-01-30 16:50:48 -0800

    adds keep hydrates function

--- file_one.js
+++ file_one.js
@@ -170,3 +170,16 @@ function aliasGen(first,last){
         return 'Your name must start with a letter from A - Z.'
     }
 }
+
+
+// Nathan loves cycling.
+// Because Nathan knows it is important to stay hydrated,
+//     he drinks 0.5 litres of water per hour of cycling.
+// You get given the time in hours and you need to return 
+// the number of litres Nathan will drink, rounded to the smallest value.
+
+
+function litres(time) {
+    let liters = time * 0.5
+    return parseInt(liters)
+}
