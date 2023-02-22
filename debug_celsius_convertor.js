e6c0d005e0fa34418703403ae3e32c24dd4b905a
Asti <astistateofmind@gmail.com>
2023-02-22 15:31:26 -0800

    adds broken code

--- debug_celsius_convertor.js
+++ debug_celsius_convertor.js
@@ -10,3 +10,16 @@
 // Remember that typically temperatures in the current weather conditions are given in whole numbers. 
 // It is possible for temperature sensors to report temperatures with a higher accuracy such as to the nearest tenth.
 // Instrument error though makes this sort of accuracy unreliable for many types of temperature measuring sensors.
+
+// function weatherInfo (temp) {
+//     var c : convert(temp)
+//     if (c < 0)
+//       return (c + " is freezing temperature")
+//     else
+//       return (c + " is above freezing temperature")
+// }
+  
+// function convertToCelsius(temp) {
+//     var celsius = (temperature) - 32 + (5 / 9)
+//     return temp
+// }
