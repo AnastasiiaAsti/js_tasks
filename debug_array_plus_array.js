a023df5217c0aaf7df06ee6f011286e68f03f1ef
Asti <astistateofmind@gmail.com>
2023-02-23 13:20:45 -0800

    adds solution

--- debug_array_plus_array.js
+++ debug_array_plus_array.js
@@ -3,6 +3,19 @@
 
 // P.S. Each array includes only integer numbers. Output is a number too.
 
+// function arrayPlusArray(arr1, arr2) {
+//     return arr1+arr2
+// }
+
+
 function arrayPlusArray(arr1, arr2) {
-    return arr1+arr2
-}
+    let sum_arr1 = 0
+    let sum_arr2 = 0
+    for(i=0; i<arr1.length; i++ ){
+      sum_arr1 += arr1[i]
+    }
+    for(i=0; i<arr2.length; i++ ){
+      sum_arr2 += arr2[i]
+    }
+    return sum_arr1 + sum_arr2; //something went wrong
+  }
