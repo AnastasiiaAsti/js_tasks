8421d94f29b589544bcdd86d136b56508fba06a4
Asti <astistateofmind@gmail.com>
2023-02-23 13:13:42 -0800

    adds solution

--- debug_switch_case.js
+++ debug_switch_case.js
@@ -16,3 +16,13 @@
 //   }
 
 
+function evalObject(value){
+    switch (value.operation) {
+        case '+': return value.a + value.b;
+        case'-': return value.a - value.b;
+        case'/': return value.a / value.b;
+        case'*': return value.a * value.b;
+        case'%': return value.a % value.b;
+        case '^': return Math.pow(value.a, value.b);
+    }
+}
