4414543f41b1ccd1d88c4a35657fc8088ee66fdc
Asti <astistateofmind@gmail.com>
2023-02-27 14:10:39 -0800

    adds solution 2

--- debug_get_planet_by_id.js
+++ debug_get_planet_by_id.js
@@ -62,4 +62,17 @@ function getPlanetName(id){
     }
     
     return name;
-  }
+}
+  
+function getPlanetName2(id){
+  return {
+    1: 'Mercury',
+    2: 'Venus',
+    3: 'Earth',
+    4: 'Mars',
+    5: 'Jupiter',
+    6: 'Saturn',
+    7: 'Uranus',
+    8: 'Neptune'
+  }[id]
+}
