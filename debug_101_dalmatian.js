f057c1668036af6f3c419a928378a68284d184e1
Asti <astistateofmind@gmail.com>
2023-03-06 17:27:59 -0800

    adds second option

--- debug_101_dalmatian.js
+++ debug_101_dalmatian.js
@@ -29,3 +29,7 @@ function howManyDalmatians(number){
     return "Woah that's a lot of dogs!" 
     }
 }
+
+const dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIANS!!!"];
+
+const howManyDalmatians2 = number  => number <= 10 ? dogs[0] : (number <= 50 ? dogs[1] : (number == 101 ? dogs[3] : dogs[2]));
