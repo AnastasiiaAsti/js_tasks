ac2128dad8add384e88b83d2756cf3851716d893
Asti <astistateofmind@gmail.com>
2023-03-06 17:26:56 -0800

    adds solution

--- debug_101_dalmatian.js
+++ debug_101_dalmatian.js
@@ -14,3 +14,18 @@
 //     var respond = number <= 10 ? dogs[0] (number <= 50 ? dogs[1] : (number = 101  dogs[3] : dogs[2]
     
 //   return respond
+
+
+// Solution
+
+function howManyDalmatians(number){
+    if (number <= 10) {
+    return "Hardly any"
+    } else if (number <= 50) {
+    return "More than a handful!"
+    } else if (number === 101) {
+    return "101 DALMATIANS!!!"
+    } else {
+    return "Woah that's a lot of dogs!" 
+    }
+}
