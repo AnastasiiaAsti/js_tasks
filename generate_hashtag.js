f5b55ae059929fd7e86404676639ffeed5685da9
Asti <astistateofmind@gmail.com>
2023-04-03 11:17:22 -0700

    adds possible solution

--- generate_hashtag.js
+++ generate_hashtag.js
@@ -7,3 +7,19 @@
 // All words must have their first letter capitalized.
 // If the final result is longer than 140 chars it must return false.
 // If the input or the result is an empty string it must return false.
+
+function generateHashtag(str) {
+    if (!str) {
+        return false
+    } else if (str == ' '){
+        return false
+    } else if (str == '#'){
+        return false
+    }
+    let capitalizedWord = str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join('')
+    let hashtag = '#' + capitalizedWord
+    if (hashtag.length > 140) {
+        return false
+    }
+    return hashtag
+}
