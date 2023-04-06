02e1ae84a8a3e91c87ff4bc81604e868cc957ee0
Asti <astistateofmind@gmail.com>
2023-04-05 17:54:16 -0700

    adds trim() instead for if else statement

--- debug_get_full_name.js
+++ debug_get_full_name.js
@@ -44,4 +44,19 @@ class Dinglemouse{
         }
       }
     
+}
+  
+// refactor
+
+class Dinglemouse{
+
+    constructor(firstName, lastName) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+    }
+    
+    getFullName() {
+      return `${this.firstName} ${this.lastName}`.trim();
+    }
+    
   }
